214fccfc47e511f9abe50d74784ffe936bea2397
Nathan Chan <nathan.chan1996@gmail.com>
2023-02-28 15:19:30 -0500

    added textDirection as required

--- lib/utils/text_utils.dart
+++ lib/utils/text_utils.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/widgets.dart';
 
 class TextUtils {
-  static Size calculateTextSize(String text, {TextStyle? style}) {
-    final TextPainter textPainter =
-        TextPainter(text: TextSpan(text: text, style: style))..layout();
+  static Size calculateTextSize(String text,
+      {TextDirection textDirection = TextDirection.ltr, TextStyle? style}) {
+    final TextPainter textPainter = TextPainter(
+        text: TextSpan(text: text, style: style), textDirection: textDirection)
+      ..layout();
     return textPainter.size;
   }
 }
