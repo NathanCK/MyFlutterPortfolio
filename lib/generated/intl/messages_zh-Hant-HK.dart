971e91f386bc5c552189e1cdf357f31f1c5bbbef
Nathan Chan <nathan.chan1996@gmail.com>
2023-03-08 04:22:09 -0500

    translation and generated files

---
+++ lib/generated/intl/messages_zh-Hant-HK.dart
@@ -0,0 +1,28 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a zh_Hant-HK locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'zh_Hant-HK';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "home_page_intro_statement": MessageLookupByLibrary.simpleMessage(
+            "I am Nathan Chan and I\'m a software engineer with a focus on mobile and backend development. Over the past two years, I have gained extensive experience in building cross-platform mobile applications using Flutter and developing backend systems using C#. My passion for technology has led me to become a dedicated and skilled software engineer, with a strong foundation in computer science and a commitment to delivering high-quality solutions.")
+      };
+}
