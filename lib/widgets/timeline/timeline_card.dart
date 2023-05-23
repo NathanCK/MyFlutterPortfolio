96e2c5f125969a35758f4e3c85cad8148df1098c
Kin (Nathan) Chan <kinnathanchan@gmail.com>
2023-05-23 01:06:05 -0400

    timeline card containing the summary of the event

---
+++ lib/widgets/timeline/timeline_card.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:my_flutter_website/models/timeline_event_model.dart';
+import 'package:my_flutter_website/utils/date_time_utils.dart';
+
+class TimelineCard extends StatelessWidget {
+  final TimelineEventModel data;
+
+  final void Function()? onTapIndexed;
+
+  const TimelineCard({
+    super.key,
+    required this.data,
+    this.onTapIndexed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (data.icon != null)
+          Center(
+            child: SizedBox(
+              width: 100,
+              height: 100,
+              child: FittedBox(
+                fit: BoxFit.fill,
+                child: data.icon!,
+              ),
+            ),
+          ),
+        Text(data.title),
+        if (data.subtitle != null) Text(data.subtitle!),
+        Text('${data.startDate.toStringMY()} - ${data.endDate.toStringMY()}'),
+        if (data.shortDescription != null)
+          Text(
+            data.shortDescription!,
+          ),
+      ],
+    );
+  }
+}
