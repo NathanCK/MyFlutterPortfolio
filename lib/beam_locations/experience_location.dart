b9d7ffeb94ab393a295bdb67f950a0e39a96d80b
Kin (Nathan) Chan <kinnathanchan@gmail.com>
2023-05-23 13:21:54 -0400

    added beam page for experience detail

--- lib/beam_locations/experience_location.dart
+++ lib/beam_locations/experience_location.dart
@@ -1,16 +1,35 @@
 import 'package:beamer/beamer.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:my_flutter_website/beam_locations/app_path.dart';
+import 'package:my_flutter_website/screens/experience_detail_screen.dart';
 import 'package:my_flutter_website/screens/experience_screen.dart';
 
 class ExperienceLocation extends BeamLocation<BeamState> {
   @override
   List<BeamPage> buildPages(BuildContext context, BeamState state) {
-    return <BeamPage>[
+    final List<BeamPage> pages = [
       const BeamPage(
-          key: ValueKey('ExperienceScreen'), child: ExperienceScreen())
+          key: ValueKey('ExperienceScreen'), child: ExperienceScreen()),
     ];
+
+    if (state.pathParameters.containsKey('experienceId')) {
+      final id = int.parse(state.pathParameters['experienceId']!);
+
+      pages.add(
+        BeamPage(
+          key: ValueKey('experienceId-$id'),
+          child: ExperienceDetailPage(id: id),
+        ),
+      );
+    }
+
+    return pages;
   }
 
   @override
-  List<Pattern> get pathPatterns => ['/experience/*'];
+  List<Pattern> get pathPatterns => [
+        AppPath.experience,
+        AppPath.experienceDetail,
+      ];
 }
