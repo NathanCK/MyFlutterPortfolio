201d493df43478fe71de9bf129df843d5658aec8
Kin (Nathan) Chan <kinnathanchan@gmail.com>
2023-05-23 01:06:18 -0400

    the timeline

---
+++ lib/widgets/timeline/timeline.dart
@@ -0,0 +1,240 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:my_flutter_website/models/timeline_event_model.dart';
+import 'package:my_flutter_website/widgets/timeline/timeline_event_box_painter.dart';
+import 'package:my_flutter_website/widgets/timeline/timeline_settings.dart';
+
+class Timeline extends StatefulWidget {
+  final List<TimelineEventModel> timelineDataList;
+  final TimelineSettings settings;
+  final Widget Function(BuildContext buildContext, int index) cardBuilder;
+  final void Function(BuildContext buildContext, int index)? onCardTap;
+  final EdgeInsetsGeometry? cardPadding;
+  final EdgeInsetsGeometry? cardMargin;
+
+  final Decoration? decoration;
+
+  const Timeline({
+    super.key,
+    required this.timelineDataList,
+    required this.cardBuilder,
+    this.settings = const TimelineSettings(),
+    this.decoration,
+    this.cardMargin,
+    this.cardPadding,
+    this.onCardTap,
+  });
+
+  @override
+  State<StatefulWidget> createState() => _TimelineState();
+}
+
+class _TimelineState extends State<Timeline> {
+  static const TextStyle _defaultMonthTextStyle =
+      TextStyle(fontSize: 10, color: Colors.black);
+  static const TextStyle _defaultYearTextStyle =
+      TextStyle(fontSize: 16, color: Colors.black);
+
+  late final ScrollController _scrollController;
+
+  int? selectedIndex;
+
+  @override
+  void initState() {
+    _scrollController = ScrollController();
+    super.initState();
+  }
+
+  Future<void> _onTapIndex(int index) async {
+    double scrollToOffset = (index) * (widget.settings.eventItemWidth) +
+        (widget.settings.eventItemWidth / 2);
+
+    await _scrollController.animateTo(scrollToOffset,
+        duration: const Duration(milliseconds: 500), curve: Curves.ease);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    final TextStyle monthTextStyle =
+        widget.settings.monthTextStyle ?? _defaultMonthTextStyle;
+    final TextStyle yearTextStyle =
+        widget.settings.yearTextStyle ?? _defaultYearTextStyle;
+
+    return Center(
+      child: SizedBox(
+        height: screenSize.height / 2,
+        child: ScrollConfiguration(
+            behavior: ScrollConfiguration.of(context).copyWith(dragDevices: {
+              PointerDeviceKind.touch,
+              PointerDeviceKind.mouse,
+              PointerDeviceKind.unknown
+            }),
+            child: Listener(
+              onPointerSignal: (PointerSignalEvent event) {
+                if (event is PointerScrollEvent) {
+                  _scrollController
+                      .jumpTo(_scrollController.offset + event.scrollDelta.dy);
+                }
+              },
+              child: CustomScrollView(
+                controller: _scrollController,
+                scrollDirection: Axis.horizontal,
+                slivers: [
+                  SliverToBoxAdapter(
+                    child:
+                        SizedBox(width: MediaQuery.of(context).size.width / 2),
+                  ),
+                  SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      (context, index) {
+                        final Widget card = widget.cardBuilder(context, index);
+
+                        final Widget outerCard = Hero(
+                          tag: widget.timelineDataList[index].title,
+                          child: Material(
+                            type: MaterialType.transparency,
+                            child: Container(
+                              width: widget.settings.eventItemWidth,
+                              decoration: widget.decoration,
+                              margin: widget.cardMargin,
+                              child: InkWell(
+                                onTap: () async {
+                                  await _onTapIndex(index);
+                                  if (widget.onCardTap != null && mounted) {
+                                    widget.onCardTap!(context, index);
+                                  }
+                                },
+                                child: Container(
+                                  padding: widget.cardPadding,
+                                  child: card,
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+
+                        return _TimelineCardHolder(
+                          data: widget.timelineDataList[index],
+                          card: outerCard,
+                          isFirst: index == 0,
+                          isLast: index == widget.timelineDataList.length - 1,
+                          cardWidth: widget.settings.eventItemWidth,
+                          width: widget.settings.eventItemWidth,
+                          timelineBarHeight: widget.settings.bottomAreaHeight,
+                          timelineGap: index.isEven
+                              ? widget.settings.connectorHeightOnEven
+                              : widget.settings.connectorHeightOnOdd,
+                          makerIconSize: widget.settings.eventMakerSize,
+                          isSelected: false,
+                          displayEventTime:
+                              widget.settings.shouldDisplayEventTime,
+                          monthTextStyle: monthTextStyle,
+                          yearTextStyle: yearTextStyle,
+                          strokeColor: widget.settings.lineColor,
+                          strokeWidth: widget.settings.lineWidth,
+                        );
+                      },
+                      childCount: widget.timelineDataList.length,
+                    ),
+                  ),
+                  SliverToBoxAdapter(
+                    child:
+                        SizedBox(width: MediaQuery.of(context).size.width / 2),
+                  ),
+                ],
+              ),
+            )),
+      ),
+    );
+  }
+}
+
+class _TimelineCardHolder extends StatelessWidget {
+  final TimelineEventModel data;
+  final Widget card;
+  final double cardWidth;
+  final bool isFirst;
+  final bool isLast;
+  final bool isSelected;
+  final TextStyle monthTextStyle;
+  final TextStyle yearTextStyle;
+  final double width;
+  final double timelineGap;
+  final double makerIconSize;
+  final double timelineBarHeight;
+  final bool displayEventTime;
+  final Color strokeColor;
+  final double strokeWidth;
+
+  const _TimelineCardHolder({
+    required this.data,
+    required this.card,
+    required this.cardWidth,
+    required this.isFirst,
+    required this.isLast,
+    required this.width,
+    required this.isSelected,
+    required this.monthTextStyle,
+    required this.yearTextStyle,
+    required this.timelineGap,
+    required this.makerIconSize,
+    required this.timelineBarHeight,
+    required this.displayEventTime,
+    required this.strokeColor,
+    required this.strokeWidth,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Align(
+          child: CustomPaint(
+            size: Size.fromWidth(width),
+            painter: TimelineEventBoxPainter(
+              date: data.startDate,
+              makerIconSize: makerIconSize,
+              timelineGap: timelineGap,
+              isFirst: isFirst,
+              isLast: isLast,
+              monthTextStyle: monthTextStyle,
+              yearTextStyle: yearTextStyle,
+              timelineBarHeight: timelineBarHeight,
+              displayEventTime: displayEventTime,
+              strokeColor: strokeColor,
+              strokeWidth: strokeWidth,
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: timelineBarHeight - (makerIconSize / 2),
+          right: 0,
+          left: 0,
+          child: SizedBox.square(
+            dimension: makerIconSize,
+            child: Align(
+              alignment: Alignment.center,
+              child: FittedBox(
+                fit: BoxFit.fill,
+                child: data.timelineDotWidget ??
+                    Icon(
+                      Icons.event,
+                      size: makerIconSize,
+                    ),
+              ),
+            ),
+          ),
+        ),
+        Positioned.directional(
+          bottom: timelineGap,
+          textDirection: TextDirection.ltr,
+          start: -cardWidth / 2 + width / 2,
+          width: cardWidth,
+          child: card,
+        ),
+      ],
+    );
+  }
+}
