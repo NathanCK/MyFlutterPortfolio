65527677d985d30ccf62594041e475fca1e5c817
Kin (Nathan) Chan <kinnathanchan@gmail.com>
2023-05-23 13:20:37 -0400

    the card for displaying the detail of a experience

---
+++ lib/widgets/timeline/timeline_detail_card.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:my_flutter_website/models/timeline_event_model.dart';
+import 'package:my_flutter_website/utils/date_time_utils.dart';
+
+class SelectedTimelineCard extends StatelessWidget {
+  final TimelineEventModel data;
+  final double width;
+  final Decoration? decoration;
+  final EdgeInsetsGeometry? padding;
+  final EdgeInsetsGeometry? margin;
+
+  const SelectedTimelineCard({
+    super.key,
+    required this.data,
+    required this.width,
+    this.decoration,
+    this.padding,
+    this.margin,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Hero(
+      tag: data.title,
+      child: Material(
+        type: MaterialType.transparency,
+        child: Container(
+          width: width,
+          padding: padding,
+          margin: margin,
+          decoration: decoration,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              if (data.icon != null)
+                Center(
+                  child: SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: FittedBox(
+                      fit: BoxFit.fill,
+                      child: data.icon!,
+                    ),
+                  ),
+                ),
+              Text(
+                data.title,
+              ),
+              if (data.subtitle != null)
+                Text(
+                  data.subtitle!,
+                ),
+              Text(
+                '${data.startDate.toStringMY()} - ${data.endDate.toStringMY()}',
+              ),
+              if (data.longDescription != null)
+                Text(
+                  data.longDescription!,
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
