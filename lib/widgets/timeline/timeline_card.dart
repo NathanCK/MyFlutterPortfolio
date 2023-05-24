c2035c1a0d13984688f3c2b8a966ce38282c2c6f
Kin (Nathan) Chan <kinnathanchan@gmail.com>
2023-05-24 16:43:57 -0400

    used different style text for different screen size

--- lib/widgets/timeline/timeline_card.dart
+++ lib/widgets/timeline/timeline_card.dart
@@ -1,17 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:my_flutter_website/enum/screen_size_type.dart';
 import 'package:my_flutter_website/models/timeline_event_model.dart';
 import 'package:my_flutter_website/utils/date_time_utils.dart';
+import 'package:my_flutter_website/utils/themed_texts.dart';
 
 class TimelineCard extends StatelessWidget {
   final TimelineEventModel data;
+  final ScreenSizeType? screenSizeType;
 
   const TimelineCard({
     super.key,
     required this.data,
+    this.screenSizeType,
   });
 
   @override
   Widget build(BuildContext context) {
+    Widget title;
+    Widget date;
+    Widget? subTitle;
+    Widget? shortDescription;
+
+    switch (screenSizeType) {
+      case ScreenSizeType.big:
+        title = HeadlineLargeText(data.title);
+        date = LabelLargeText(
+            '${data.startDate.toStringMY()} - ${data.endDate.toStringMY()}');
+        if (data.subtitle != null) subTitle = TitleLargeText(data.subtitle!);
+        if (data.shortDescription != null) {
+          shortDescription = BodyLargeText(data.subtitle!);
+        }
+        break;
+      case ScreenSizeType.medium:
+        title = HeadlineMediumText(data.title);
+        if (data.subtitle != null) subTitle = TitleMediumText(data.subtitle!);
+        date = LabelMediumText(
+            '${data.startDate.toStringMY()} - ${data.endDate.toStringMY()}');
+        if (data.shortDescription != null) {
+          shortDescription = BodyMediumText(data.subtitle!);
+        }
+        break;
+      case null:
+      case ScreenSizeType.small:
+      default:
+        title = HeadlineSmallText(data.title);
+        date = LabelSmallText(
+            '${data.startDate.toStringMY()} - ${data.endDate.toStringMY()}');
+        if (data.subtitle != null) subTitle = TitleSmallText(data.subtitle!);
+        if (data.shortDescription != null) {
+          shortDescription = BodySmallText(data.subtitle!);
+        }
+    }
+
     return Column(
       mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: MainAxisAlignment.start,
@@ -24,13 +64,10 @@ class TimelineCard extends StatelessWidget {
               child: data.icon!,
             ),
           ),
-        Text(data.title),
-        if (data.subtitle != null) Text(data.subtitle!),
-        Text('${data.startDate.toStringMY()} - ${data.endDate.toStringMY()}'),
-        if (data.shortDescription != null)
-          Text(
-            data.shortDescription!,
-          ),
+        title,
+        if (subTitle != null) subTitle,
+        date,
+        if (shortDescription != null) shortDescription,
       ],
     );
   }
