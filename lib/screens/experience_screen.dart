d040d5bc8182e7db36ac94839d63295cd649acef
Kin (Nathan) Chan <kinnathanchan@gmail.com>
2023-05-24 16:45:01 -0400

    calculate the screen type for different layout

--- lib/screens/experience_screen.dart
+++ lib/screens/experience_screen.dart
@@ -1,6 +1,7 @@
 import 'package:beamer/beamer.dart';
 import 'package:flutter/material.dart';
 import 'package:my_flutter_website/beam_locations/app_path.dart';
+import 'package:my_flutter_website/enum/screen_size_type.dart';
 import 'package:my_flutter_website/utils/my_experience_data.dart';
 import 'package:my_flutter_website/widgets/timeline/timeline.dart';
 import 'package:my_flutter_website/widgets/timeline/timeline_card.dart';
@@ -12,26 +13,48 @@ class ExperienceScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.transparent,
-      body: Timeline(
-        cardBuilder: (buildContext, index) {
-          return TimelineCard(
-            data: timelineEventModels[index],
-          );
-        },
-        onCardTap: (context, index) {
-          context.beamToNamed(
-              AppPath.getExperienceDetailUri(timelineEventModels[index].id));
-        },
-        timelineDataList: timelineEventModels,
-        decoration: BoxDecoration(
-          shape: BoxShape.rectangle,
-          borderRadius: const BorderRadius.all(Radius.circular(10)),
-          color: const Color.fromARGB(255, 219, 202, 202),
-          border: Border.all(),
-        ),
-        cardMargin: const EdgeInsets.only(left: 8, right: 8, bottom: 2),
-        cardPadding: const EdgeInsets.all(8),
-      ),
+      body: LayoutBuilder(builder: (context, boxConstraints) {
+        /// TODO: maybe can merge the logic with home screen, and put the screen
+        /// type in the app status.
+        final screenSizeType = _getScreenSizeType(
+            screenHeight: boxConstraints.maxHeight,
+            screenWidth: boxConstraints.maxWidth);
+
+        return Timeline(
+          cardBuilder: (context, index) {
+            return TimelineCard(
+              data: timelineEventModels[index],
+              screenSizeType: screenSizeType,
+            );
+          },
+          onCardTap: (context, index) {
+            context.beamToNamed(
+                AppPath.getExperienceDetailUri(timelineEventModels[index].id));
+          },
+          timelineDataList: timelineEventModels,
+          decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            borderRadius: const BorderRadius.all(Radius.circular(10)),
+            color: const Color.fromARGB(255, 219, 202, 202),
+            border: Border.all(),
+          ),
+          cardMargin: const EdgeInsets.only(left: 8, right: 8, bottom: 2),
+          cardPadding: const EdgeInsets.all(8),
+        );
+      }),
     );
+  }
+
+  ScreenSizeType _getScreenSizeType(
+      {required double screenHeight, required double screenWidth}) {
+    if (screenHeight <= 400 || screenWidth <= 412) {
+      return ScreenSizeType.small;
+    }
+
+    if (screenWidth >= 820) {
+      return ScreenSizeType.big;
+    }
+
+    return ScreenSizeType.medium;
   }
 }
