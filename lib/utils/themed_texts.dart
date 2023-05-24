cb68e34cd8f927e7bc852075b313d025b0e1d305
Kin (Nathan) Chan <kinnathanchan@gmail.com>
2023-05-24 16:43:33 -0400

    added different styles of text

--- lib/utils/themed_texts.dart
+++ lib/utils/themed_texts.dart
@@ -28,10 +28,10 @@ class DisplayLargeText extends ThemedText {
 
   @override
   Widget build(BuildContext context) {
-    final titleStyle = Theme.of(context).primaryTextTheme.displayLarge!;
+    final style = Theme.of(context).primaryTextTheme.displayLarge!;
     return AutoSizeText(
       text,
-      style: titleStyle.copyWith(overflow: overflow),
+      style: style.copyWith(overflow: overflow),
       textAlign: textAlign,
       maxLines: maxLines,
       maxFontSize: maxFontSize,
@@ -54,10 +54,10 @@ class DisplayMediumText extends ThemedText {
 
   @override
   Widget build(BuildContext context) {
-    final titleStyle = Theme.of(context).primaryTextTheme.displayMedium!;
+    final style = Theme.of(context).primaryTextTheme.displayMedium!;
     return AutoSizeText(
       text,
-      style: titleStyle.copyWith(overflow: overflow),
+      style: style.copyWith(overflow: overflow),
       textAlign: textAlign,
       maxLines: maxLines,
       maxFontSize: maxFontSize,
@@ -80,10 +80,10 @@ class DisplaySmallText extends ThemedText {
 
   @override
   Widget build(BuildContext context) {
-    final titleStyle = Theme.of(context).primaryTextTheme.displaySmall!;
+    final style = Theme.of(context).primaryTextTheme.displaySmall!;
     return AutoSizeText(
       text,
-      style: titleStyle.copyWith(overflow: overflow),
+      style: style.copyWith(overflow: overflow),
       textAlign: textAlign,
       maxLines: maxLines,
       maxFontSize: maxFontSize,
@@ -108,14 +108,306 @@ class BodyLargeText extends ThemedText {
 
   @override
   Widget build(BuildContext context) {
-    final titleStyle = Theme.of(context).primaryTextTheme.bodyLarge!;
+    final style = Theme.of(context).primaryTextTheme.bodyLarge!;
+    return AutoSizeText(
+      text,
+      style: style.copyWith(overflow: overflow),
+      textAlign: textAlign,
+      maxLines: maxLines,
+      maxFontSize: maxFontSize,
+      stepGranularity: stepGranularity,
+    );
+  }
+}
+
+class BodyMediumText extends ThemedText {
+  final TextAlign? textAlign;
+  final int? maxLines;
+  final TextOverflow? overflow;
+  final double maxFontSize;
+  final double stepGranularity;
+
+  const BodyMediumText(super.text,
+      {this.textAlign,
+      this.maxLines,
+      this.overflow,
+      this.maxFontSize = double.infinity,
+      this.stepGranularity = 1,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final style = Theme.of(context).primaryTextTheme.bodyMedium!;
+    return AutoSizeText(
+      text,
+      style: style.copyWith(overflow: overflow),
+      textAlign: textAlign,
+      maxLines: maxLines,
+      maxFontSize: maxFontSize,
+      stepGranularity: stepGranularity,
+    );
+  }
+}
+
+class BodySmallText extends ThemedText {
+  final TextAlign? textAlign;
+  final int? maxLines;
+  final TextOverflow? overflow;
+  final double maxFontSize;
+  final double stepGranularity;
+
+  const BodySmallText(super.text,
+      {this.textAlign,
+      this.maxLines,
+      this.overflow,
+      this.maxFontSize = double.infinity,
+      this.stepGranularity = 1,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final style = Theme.of(context).primaryTextTheme.bodySmall!;
+    return AutoSizeText(
+      text,
+      style: style.copyWith(overflow: overflow),
+      textAlign: textAlign,
+      maxLines: maxLines,
+      maxFontSize: maxFontSize,
+      stepGranularity: stepGranularity,
+    );
+  }
+}
+
+class TitleLargeText extends ThemedText {
+  final TextAlign? textAlign;
+  final int? maxLines;
+  final TextOverflow? overflow;
+  final double maxFontSize;
+
+  const TitleLargeText(super.text,
+      {this.textAlign,
+      this.maxLines,
+      this.overflow,
+      this.maxFontSize = double.infinity,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final titleStyle = Theme.of(context).primaryTextTheme.titleLarge!;
     return AutoSizeText(
       text,
       style: titleStyle.copyWith(overflow: overflow),
       textAlign: textAlign,
       maxLines: maxLines,
       maxFontSize: maxFontSize,
-      stepGranularity: stepGranularity,
+    );
+  }
+}
+
+class TitleMediumText extends ThemedText {
+  final TextAlign? textAlign;
+  final int? maxLines;
+  final TextOverflow? overflow;
+  final double maxFontSize;
+
+  const TitleMediumText(super.text,
+      {this.textAlign,
+      this.maxLines,
+      this.overflow,
+      this.maxFontSize = double.infinity,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final titleStyle = Theme.of(context).primaryTextTheme.titleMedium!;
+    return AutoSizeText(
+      text,
+      style: titleStyle.copyWith(overflow: overflow),
+      textAlign: textAlign,
+      maxLines: maxLines,
+      maxFontSize: maxFontSize,
+    );
+  }
+}
+
+class TitleSmallText extends ThemedText {
+  final TextAlign? textAlign;
+  final int? maxLines;
+  final TextOverflow? overflow;
+  final double maxFontSize;
+
+  const TitleSmallText(super.text,
+      {this.textAlign,
+      this.maxLines,
+      this.overflow,
+      this.maxFontSize = double.infinity,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final titleStyle = Theme.of(context).primaryTextTheme.titleSmall!;
+    return AutoSizeText(
+      text,
+      style: titleStyle.copyWith(overflow: overflow),
+      textAlign: textAlign,
+      maxLines: maxLines,
+      maxFontSize: maxFontSize,
+    );
+  }
+}
+
+class HeadlineLargeText extends ThemedText {
+  final TextAlign? textAlign;
+  final int? maxLines;
+  final TextOverflow? overflow;
+  final double maxFontSize;
+
+  const HeadlineLargeText(super.text,
+      {this.textAlign,
+      this.maxLines,
+      this.overflow,
+      this.maxFontSize = double.infinity,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final style = Theme.of(context).primaryTextTheme.headlineLarge!;
+    return AutoSizeText(
+      text,
+      style: style.copyWith(overflow: overflow),
+      textAlign: textAlign,
+      maxLines: maxLines,
+      maxFontSize: maxFontSize,
+    );
+  }
+}
+
+class HeadlineMediumText extends ThemedText {
+  final TextAlign? textAlign;
+  final int? maxLines;
+  final TextOverflow? overflow;
+  final double maxFontSize;
+
+  const HeadlineMediumText(super.text,
+      {this.textAlign,
+      this.maxLines,
+      this.overflow,
+      this.maxFontSize = double.infinity,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final style = Theme.of(context).primaryTextTheme.headlineMedium!;
+    return AutoSizeText(
+      text,
+      style: style.copyWith(overflow: overflow),
+      textAlign: textAlign,
+      maxLines: maxLines,
+      maxFontSize: maxFontSize,
+    );
+  }
+}
+
+class HeadlineSmallText extends ThemedText {
+  final TextAlign? textAlign;
+  final int? maxLines;
+  final TextOverflow? overflow;
+  final double maxFontSize;
+
+  const HeadlineSmallText(super.text,
+      {this.textAlign,
+      this.maxLines,
+      this.overflow,
+      this.maxFontSize = double.infinity,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final style = Theme.of(context).primaryTextTheme.headlineSmall!;
+    return AutoSizeText(
+      text,
+      style: style.copyWith(overflow: overflow),
+      textAlign: textAlign,
+      maxLines: maxLines,
+      maxFontSize: maxFontSize,
+    );
+  }
+}
+
+class LabelLargeText extends ThemedText {
+  final TextAlign? textAlign;
+  final int? maxLines;
+  final TextOverflow? overflow;
+  final double maxFontSize;
+
+  const LabelLargeText(super.text,
+      {this.textAlign,
+      this.maxLines,
+      this.overflow,
+      this.maxFontSize = double.infinity,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final style = Theme.of(context).primaryTextTheme.labelLarge!;
+    return AutoSizeText(
+      text,
+      style: style.copyWith(overflow: overflow),
+      textAlign: textAlign,
+      maxLines: maxLines,
+      maxFontSize: maxFontSize,
+    );
+  }
+}
+
+class LabelMediumText extends ThemedText {
+  final TextAlign? textAlign;
+  final int? maxLines;
+  final TextOverflow? overflow;
+  final double maxFontSize;
+
+  const LabelMediumText(super.text,
+      {this.textAlign,
+      this.maxLines,
+      this.overflow,
+      this.maxFontSize = double.infinity,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final style = Theme.of(context).primaryTextTheme.labelMedium!;
+    return AutoSizeText(
+      text,
+      style: style.copyWith(overflow: overflow),
+      textAlign: textAlign,
+      maxLines: maxLines,
+      maxFontSize: maxFontSize,
+    );
+  }
+}
+
+class LabelSmallText extends ThemedText {
+  final TextAlign? textAlign;
+  final int? maxLines;
+  final TextOverflow? overflow;
+  final double maxFontSize;
+
+  const LabelSmallText(super.text,
+      {this.textAlign,
+      this.maxLines,
+      this.overflow,
+      this.maxFontSize = double.infinity,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final style = Theme.of(context).primaryTextTheme.labelSmall!;
+    return AutoSizeText(
+      text,
+      style: style.copyWith(overflow: overflow),
+      textAlign: textAlign,
+      maxLines: maxLines,
+      maxFontSize: maxFontSize,
     );
   }
 }
